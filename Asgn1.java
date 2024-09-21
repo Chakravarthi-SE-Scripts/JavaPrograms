5099e303b2de1fa30a5dd96db14187f28a9a0f2e
Chakravarthi-SE-Scripts <chakravarthikalavagunta@gmail.com>
2024-09-21 10:35:15 +0530

    committed

---
+++ Asgn1.java
@@ -0,0 +1,39 @@
+
+public class Asgn1 {
+
+	static void method1()
+	{
+		System.out.println("Static 1");
+	}
+
+	static void method2()
+	{
+		System.out.println("Static 2");
+	}
+	static void method3()
+	{
+		System.out.println("Static 3");
+	}
+	static void method4()
+	{
+		System.out.println("Static 4");
+	}
+	static void method5()
+	{
+		System.out.println("Static 5");
+	}
+	
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		method1();
+		method2();
+		method3();
+		method4();
+		method5();
+		
+
+	}
+
+}
